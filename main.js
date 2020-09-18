9803f9f3fdca9f461cb21ce7132d828115e128f6
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-18 11:08:58 +0200

    aggiunto: funzione render() / genere  come data-attribute a elemento cd

--- main.js
+++ main.js
@@ -10,18 +10,8 @@ $(document).ready(function() {
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
     success:  function (data, stato) {
-                // recupera i dati dell'oggetto disco che risponde l'API
-                var cd = data.response;
-                for (var i = 0; i < cd.length; i++) {
-                  // recupera l'html del template
-                  var source = $("#cd-template").html();
-                  // compila il template
-                  var template = Handlebars.compile(source);
-                  // manipola i fermaposto del template passando l'oggetto cd[i] e crea il codice HTML da iniettare
-                  var html = template(cd[i]);
-                  // inietta il codice nell'elemento scelto
-                  $(".cds-container").append(html);
-                }
+              // chima la funzione per fare il render con Handlebars
+              render(data.response);
               },
     error: function (richiesta, stato, errore) {
             // messaggio di errore
@@ -35,24 +25,35 @@ $(document).ready(function() {
     function(){
       // recupera il genere musicale scelto
       var genereScelto = $(this).val();
-      // nasconde tutti i cd
-      $(".cd").hide();
       // mostra il cd a seconda del genere musicale scelto
-      $(".cd .genre").each(function () {
-          if (genereScelto == "All Genres") {
-            // ALL GENRES mostra tutti i cd
-            $(this).parent().fadeIn();
-          } else if ($(this).text() == genereScelto) {
-            // mostra solo i cd del genere scelto
-            $(this).parent().fadeIn();
-          }
-        }
-      );
+      if (genereScelto == "") {
+        // è stato scelto All Genres / mostra tutti i cd
+        $(".cd").fadeIn();
+      } else {
+        // nasconde tutti i cd
+        $(".cd").hide();
+        // mostra solo i cd del genere scelto
+        $(".cd[data-genere='" + genereScelto + "']").fadeIn()
+      }
     }
   );
 
 
-
-
 //ATTENZIONE: non scrivere codice sotto questa riga
 });
+
+// funzione di rendering
+
+function render(data) {
+  // recupera l'html del template
+  var source = $("#cd-template").html();
+  // compila il template
+  var template = Handlebars.compile(source);
+  // ciclo per creare tanti cd quanti sono gli oggetti dell'array che ci torna l'Api
+  for (var i = 0; i < data.length; i++) {
+    // manipola i fermaposto del template passando l'oggetto cd[i] e crea il codice HTML da iniettare
+    var html = template(data[i]);
+    // inietta il codice nell'elemento scelto
+    $(".cds-container").append(html);
+  }
+}
